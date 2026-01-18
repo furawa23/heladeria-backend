68ca301ddfa057eef7ea3117e427560551898729
furawa23 <fabrizzioflores2014@gmail.com>
2026-01-17 22:45:52 -0500

    agregado entitygraph a repo de producto, antes solo a seguridad

--- src/main/java/com/togamma/heladeria/repository/almacen/ProductoRepository.java
+++ src/main/java/com/togamma/heladeria/repository/almacen/ProductoRepository.java
@@ -1,15 +1,27 @@
 package com.togamma.heladeria.repository.almacen;
 
+import java.util.Optional;
+
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import com.togamma.heladeria.model.almacen.Producto;
 import com.togamma.heladeria.repository.EmpresaScopedRepository;
 
 public interface ProductoRepository extends EmpresaScopedRepository<Producto> {
     
+    @EntityGraph(attributePaths = {"categoria"})
     Page<Producto> findByEmpresaIdAndSeVendeTrue(Long id, Pageable pageable);
+    @EntityGraph(attributePaths = {"categoria"})
     Page<Producto> findByEmpresaIdAndSeVendeFalse(Long id, Pageable pageable);
     Boolean existsByNombreAndEmpresaId(String nombre, Long id);
+    @EntityGraph(attributePaths = {"categoria"})
     Page<Producto> findByCategoriaIdAndEmpresaId(Long idCategoria, Long idEmpresa, Pageable pageable);
+    @Override
+    @EntityGraph(attributePaths = {"categoria"})
+    Page<Producto> findByEmpresaId(Long idEmpresa, Pageable pageable);
+    @Override
+    @EntityGraph(attributePaths = {"categoria", "receta.insumo"})
+    Optional<Producto> findByIdAndEmpresaId(Long id, Long idEmpresa);
 }
