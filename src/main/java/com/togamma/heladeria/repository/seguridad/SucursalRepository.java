4111cf2bc4d11d702846da20d50e97bb8ed4da8e
furawa23 <fabrizzioflores2014@gmail.com>
2026-01-17 22:33:23 -0500

    entitygraph agregado para optimizar consultas

--- src/main/java/com/togamma/heladeria/repository/seguridad/SucursalRepository.java
+++ src/main/java/com/togamma/heladeria/repository/seguridad/SucursalRepository.java
@@ -1,8 +1,25 @@
 package com.togamma.heladeria.repository.seguridad;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+
 import com.togamma.heladeria.model.seguridad.Sucursal;
 import com.togamma.heladeria.repository.EmpresaScopedRepository;
 
 public interface SucursalRepository extends EmpresaScopedRepository<Sucursal> {
 
+    @Override
+    @EntityGraph(attributePaths = {"empresa"})
+    Page<Sucursal> findAll(Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {"empresa"})
+    Page<Sucursal> findByEmpresaId(Long idEmpresa, Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {"empresa"})
+    Optional<Sucursal> findById(Long id);
 }
--- src/main/java/com/togamma/heladeria/repository/seguridad/UsuarioRepository.java
+++ src/main/java/com/togamma/heladeria/repository/seguridad/UsuarioRepository.java
@@ -4,12 +4,24 @@ import java.util.Optional;
 
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
 
 import com.togamma.heladeria.model.seguridad.Usuario;
 import com.togamma.heladeria.repository.SucursalScopedRepository;
 
 public interface UsuarioRepository extends SucursalScopedRepository<Usuario> {
-
+    
+    @Override
+    @EntityGraph(attributePaths = {"sucursal", "empresa"})
+    Page<Usuario> findAll(Pageable pageable);
+    @Override
+    @EntityGraph(attributePaths = {"sucursal", "empresa"})
+    Optional<Usuario> findById(Long id);
+    @Override
+    @EntityGraph(attributePaths = {"sucursal", "empresa"})
+    Page<Usuario> findBySucursalId(Long idSucursal, Pageable pageable);
+    @EntityGraph(attributePaths = {"sucursal", "empresa"})
     Optional<Usuario> findByUsername(String username);
+    @EntityGraph(attributePaths = {"sucursal", "empresa"})
     Page<Usuario> findBySucursalEmpresaId(Long idEmpresa, Pageable pageable);
 }
