24d700c2a3b45f8a6c4fa389272bbd24e279e998
furawa23 <fabrizzioflores2014@gmail.com>
2026-03-01 19:14:06 -0500

    repos de carpeta compra ordenados

--- src/main/java/com/togamma/heladeria/repository/compra/CompraRepository.java
+++ src/main/java/com/togamma/heladeria/repository/compra/CompraRepository.java
@@ -1,9 +1,23 @@
 package com.togamma.heladeria.repository.compra;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+
 import com.togamma.heladeria.model.compra.Compra;
 import com.togamma.heladeria.repository.SucursalScopedRepository;
 
 public interface CompraRepository extends SucursalScopedRepository<Compra> {
+    
     Boolean existsByNumeroComprobanteAndSucursalId(String numeroComprobante, Long id);
 
+    @Override
+    @EntityGraph(attributePaths = {"proveedor", "detalles.producto", "detalles.presentacion"})
+    Optional<Compra> findByIdAndSucursalId(Long id, Long idSucursal);
+
+    @Override
+    @EntityGraph(attributePaths = {"proveedor", "detalles.producto", "detalles.presentacion"})
+    Page<Compra> findBySucursalId(Long idSucursal, Pageable pageable);
 }
