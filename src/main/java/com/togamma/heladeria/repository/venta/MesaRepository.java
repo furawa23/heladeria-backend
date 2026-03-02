e83b186c06f1d37f7dbd94e09d220c82f1f6d68e
furawa23 <fabrizzioflores2014@gmail.com>
2026-03-01 19:37:28 -0500

    repos de carpeta de venta ordenados

--- src/main/java/com/togamma/heladeria/repository/venta/MesaRepository.java
+++ src/main/java/com/togamma/heladeria/repository/venta/MesaRepository.java
@@ -1,5 +1,11 @@
 package com.togamma.heladeria.repository.venta;
 
+import java.util.Optional;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.jpa.repository.EntityGraph;
+
 import com.togamma.heladeria.model.venta.Mesa;
 import com.togamma.heladeria.repository.SucursalScopedRepository;
 
@@ -7,4 +13,11 @@ public interface MesaRepository extends SucursalScopedRepository<Mesa> {
 
     boolean existsByNumeroAndSucursalId(Integer numero, Long id);
 
+    @Override
+    @EntityGraph(attributePaths = {"sucursal"})
+    Page<Mesa> findBySucursalId(Long idSucursal, Pageable pageable);
+
+    @Override
+    @EntityGraph(attributePaths = {"sucursal"})
+    Optional<Mesa> findByIdAndSucursalId(Long id, Long idSucursal);
 }
