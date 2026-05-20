e6c2ac5a3c4326a1760ff9cb80ec9c1a0b6a5bf9
furawa23 <fabrizzioflores2014@gmail.com>
2026-05-20 15:56:27 -0500

    permitir ip de vps

--- src/main/java/com/togamma/heladeria/config/SecurityConfig.java
+++ src/main/java/com/togamma/heladeria/config/SecurityConfig.java
@@ -85,7 +85,7 @@ public class SecurityConfig {
     @Bean
     CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("http://localhost:4200"));
+        configuration.setAllowedOrigins(List.of("http://localhost:4200", "http://178.238.237.71:4323"));
         configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("*"));
         configuration.setAllowCredentials(true);
